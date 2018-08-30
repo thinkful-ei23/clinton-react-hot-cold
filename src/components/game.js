1c6423ebecf42966045ae82af842a046eeb7d497
Clinton Owen <clint@clintonowen.com>
2018-08-30 15:30:54 -0500

    guess validation and feedback working

--- src/components/game.js
+++ src/components/game.js
@@ -21,21 +21,59 @@ export default class Game extends React.Component {
 
   handleGuess(e) {
     e.preventDefault();
-    this.setState({
-      guesses: [...this.state.guesses, this.state.currentGuess]
-    });
-    if (this.state.secretNumber === Number(this.state.currentGuess)) {
+    if (this.validateGuess()) {
       this.setState({
-        feedback: 'You Won. Click new game to play again'
+        guesses: [...this.state.guesses, this.state.currentGuess]
       });
-    } else {
-      this.setState({
-        feedback: 'Wrong. Guess again'
-      });
+      this.generateFeedback();
     }
     this.setState({
       currentGuess: ''
     })
+  }
+
+  validateGuess() {
+    if (this.state.currentGuess % 1 !== 0) {
+      alert('Please input a number');
+      return false;
+    } else if (this.state.currentGuess < 0 || this.state.currentGuess > 100) {
+      alert('Please choose a number between zero and 100');
+      return false;
+    } else if (this.state.guesses.length > 0) {
+      let alreadyGuessed = true;
+      this.state.guesses.forEach(guess => {
+        if (Number(this.state.currentGuess) == guess) {
+          alert('You guessed this number already');
+          alreadyGuessed = false;
+        }
+      })
+      return alreadyGuessed;
+    }
+    return true;
+  }
+
+  generateFeedback() {
+    if(this.state.secretNumber === Number(this.state.currentGuess)){
+      this.setState({
+        feedback: 'You Won. Click new game to play again'
+      });
+    } else if(Math.abs(this.state.secretNumber - Number(this.state.currentGuess)) < 10){
+      this.setState({
+        feedback: 'Hot'
+      });
+    } else if(Math.abs(this.state.secretNumber - Number(this.state.currentGuess)) < 20 && Math.abs(this.state.secretNumber - Number(this.state.currentGuess)) > 9){
+      this.setState({
+        feedback: 'Kinda hot'
+      });
+    } else if(Math.abs(this.state.secretNumber - Number(this.state.currentGuess)) < 30 && Math.abs(this.state.secretNumber - Number(this.state.currentGuess)) > 19){
+      this.setState({
+        feedback: 'Less than warm'
+      });
+    } else {
+      this.setState({
+        feedback: 'Cold'
+      });
+    }
   }
 
   onChange(e) {
